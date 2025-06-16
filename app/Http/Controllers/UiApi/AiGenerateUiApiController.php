aaf8b23193b0f95d01dfa0517fe263c27416c03f
andriimoroz <moro97andrze@gmail.com>
2025-06-16 12:20:19 +0200

    fix: system prompt overrride

--- app/Http/Controllers/UiApi/AiGenerateUiApiController.php
+++ app/Http/Controllers/UiApi/AiGenerateUiApiController.php
@@ -11,6 +11,7 @@ use Gptsdk\Compilers\DoubleBracketsPromptCompiler;
 use Gptsdk\Enum\CompilerType;
 use Gptsdk\Storage\GithubPromptStorage;
 use Gptsdk\Types\AiRequest;
+use Gptsdk\Types\Prompt;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpClient\HttpClient;
@@ -51,13 +52,23 @@ class AiGenerateUiApiController
             $githubPromptStorage
         );
 
+        $promptArray = json_decode(
+            '{"messages":[{"role":"user","content":"Generate a content for file [[filePath]]. \nIf file ends with .prompt it should be a prompt. \nIf file ends with .md it should be a documentation file.\nHere is a context\n\"\"\"\n[[context]]\n\"\"\"\nHere is an actual file content:\n\"\"\"\n[[content]]\n\"\"\""}],"variables":[{"name":"filePath","value":"","type":"string"},{"name":"context","value":"","type":"text"},{"name":"content","value":"","type":"text"}]}',
+            true
+        );
+
         $aiResponses = $completionAi->complete([
             new AiRequest(
                 apiKey: $aiApiKey->key,
                 aiVendor: $aiApiKey->ai_vendor,
                 llmOptions: ['model' => $aiApiKey->default_model],
                 compilerType: CompilerType::DOUBLE_BRACKETS,
-                promptPath: 'system/generator.prompt',
+                prompt: new Prompt(
+                    path: '',
+                    messages: $promptArray['messages'],
+                    variables: $promptArray['variables'],
+                    compilerType: CompilerType::DOUBLE_BRACKETS,
+                ),
                 variableValues: $request->get('values')
             )
         ]);
